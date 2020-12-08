04182e2025e08c9ba6ce932c3ac110e7ed1aa81d
butirpadi <butirpadi@gmail.com>
2020-12-08 12:39:04 +0700

    add grouop to account.financial.report

--- models/account_financial_report.py
+++ models/account_financial_report.py
@@ -9,4 +9,10 @@ class AccountFinancialReport(models.Model):
         string='Represents an account', default=False)
     represent_account_id = fields.Many2one(
         comodel_name='account.account', string='Represented Account')
-    post_to = fields.Selection([("lr","Labar/Rugi"),("neraca","Neraca Akhir")], string='Post to', default="neraca")
+    post_to = fields.Selection(
+        [("lr", "Labar/Rugi"), ("neraca", "Neraca Akhir")], string='Post to', default="neraca")
+    report_group_id = fields.Many2one(
+        comodel_name='account.financial.report',
+        string='Report Group',
+        ondelete='restrict'
+    )
